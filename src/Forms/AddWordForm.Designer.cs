d00e8ee5ff0e1ad43dccb01d28df5c1e6a020db3
Ferit Yiğit BALABAN <ferityigitbalaban@gmail.com>
2021-09-06 22:30:18 +0300

    Add/Edit Word Forms with ability to add multiple article links

--- src/Forms/AddWordForm.Designer.cs
+++ src/Forms/AddWordForm.Designer.cs
@@ -33,9 +33,7 @@ namespace sozluk
             this.BtnAdd = new System.Windows.Forms.Button();
             this.BtnCancel = new System.Windows.Forms.Button();
             this.TxtWordName = new System.Windows.Forms.TextBox();
-            this.TxtArticleLink = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.TxtDefinition = new System.Windows.Forms.TextBox();
@@ -48,9 +46,15 @@ namespace sozluk
             this.BtnRemoveReference = new System.Windows.Forms.Button();
             this.BtnAddReference = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.TxtArticle = new System.Windows.Forms.TextBox();
+            this.ListArticles = new System.Windows.Forms.ListBox();
+            this.BtnRemoveArticle = new System.Windows.Forms.Button();
+            this.BtnAddArticle = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnAdd
@@ -58,7 +62,7 @@ namespace sozluk
             this.BtnAdd.BackColor = System.Drawing.Color.Black;
             this.BtnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnAdd.ForeColor = System.Drawing.Color.White;
-            this.BtnAdd.Location = new System.Drawing.Point(340, 481);
+            this.BtnAdd.Location = new System.Drawing.Point(340, 656);
             this.BtnAdd.Name = "BtnAdd";
             this.BtnAdd.Size = new System.Drawing.Size(75, 23);
             this.BtnAdd.TabIndex = 0;
@@ -72,7 +76,7 @@ namespace sozluk
             this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.BtnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BtnCancel.ForeColor = System.Drawing.Color.White;
-            this.BtnCancel.Location = new System.Drawing.Point(421, 481);
+            this.BtnCancel.Location = new System.Drawing.Point(421, 656);
             this.BtnCancel.Name = "BtnCancel";
             this.BtnCancel.Size = new System.Drawing.Size(75, 23);
             this.BtnCancel.TabIndex = 1;
@@ -90,44 +94,22 @@ namespace sozluk
             this.TxtWordName.Size = new System.Drawing.Size(394, 23);
             this.TxtWordName.TabIndex = 2;
             // 
-            // TxtArticleLink
-            // 
-            this.TxtArticleLink.BackColor = System.Drawing.Color.Black;
-            this.TxtArticleLink.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.TxtArticleLink.ForeColor = System.Drawing.Color.White;
-            this.TxtArticleLink.Location = new System.Drawing.Point(84, 44);
-            this.TxtArticleLink.Name = "TxtArticleLink";
-            this.TxtArticleLink.PlaceholderText = "Article link here";
-            this.TxtArticleLink.Size = new System.Drawing.Size(394, 23);
-            this.TxtArticleLink.TabIndex = 3;
-            // 
-            // label1
+            // InnerLabel
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(10, 17);
-            this.label1.Name = "label1";
+            this.label1.Name = "InnerLabel";
             this.label1.Size = new System.Drawing.Size(72, 15);
             this.label1.TabIndex = 12;
             this.label1.Text = "Word name:";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 15);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "Article link:";
-            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.TxtArticleLink);
-            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.TxtWordName);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(484, 75);
+            this.groupBox1.Size = new System.Drawing.Size(484, 47);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
             // 
@@ -137,7 +119,7 @@ namespace sozluk
             this.groupBox2.Controls.Add(this.ListDefinitions);
             this.groupBox2.Controls.Add(this.BtnRemoveDefinition);
             this.groupBox2.Controls.Add(this.BtnAddDefinition);
-            this.groupBox2.Location = new System.Drawing.Point(12, 78);
+            this.groupBox2.Location = new System.Drawing.Point(12, 54);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(484, 193);
             this.groupBox2.TabIndex = 15;
@@ -201,7 +183,7 @@ namespace sozluk
             this.groupBox3.Controls.Add(this.ListReferences);
             this.groupBox3.Controls.Add(this.BtnRemoveReference);
             this.groupBox3.Controls.Add(this.BtnAddReference);
-            this.groupBox3.Location = new System.Drawing.Point(12, 277);
+            this.groupBox3.Location = new System.Drawing.Point(12, 452);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(232, 198);
             this.groupBox3.TabIndex = 16;
@@ -261,11 +243,75 @@ namespace sozluk
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(250, 284);
+            this.label3.Location = new System.Drawing.Point(250, 459);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(246, 187);
             this.label3.TabIndex = 17;
             this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.TxtArticle);
+            this.groupBox4.Controls.Add(this.ListArticles);
+            this.groupBox4.Controls.Add(this.BtnRemoveArticle);
+            this.groupBox4.Controls.Add(this.BtnAddArticle);
+            this.groupBox4.Location = new System.Drawing.Point(12, 253);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(484, 193);
+            this.groupBox4.TabIndex = 18;
+            this.groupBox4.TabStop = false;
+            // 
+            // TxtArticle
+            // 
+            this.TxtArticle.BackColor = System.Drawing.Color.Black;
+            this.TxtArticle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtArticle.ForeColor = System.Drawing.Color.White;
+            this.TxtArticle.Location = new System.Drawing.Point(10, 18);
+            this.TxtArticle.Multiline = true;
+            this.TxtArticle.Name = "TxtArticle";
+            this.TxtArticle.PlaceholderText = "Enter article link here";
+            this.TxtArticle.Size = new System.Drawing.Size(468, 23);
+            this.TxtArticle.TabIndex = 11;
+            // 
+            // ListArticles
+            // 
+            this.ListArticles.BackColor = System.Drawing.Color.Black;
+            this.ListArticles.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ListArticles.ForeColor = System.Drawing.Color.White;
+            this.ListArticles.ItemHeight = 15;
+            this.ListArticles.Location = new System.Drawing.Point(10, 47);
+            this.ListArticles.Name = "ListArticles";
+            this.ListArticles.Size = new System.Drawing.Size(468, 107);
+            this.ListArticles.TabIndex = 10;
+            // 
+            // BtnRemoveArticle
+            // 
+            this.BtnRemoveArticle.BackColor = System.Drawing.Color.Black;
+            this.BtnRemoveArticle.Enabled = false;
+            this.BtnRemoveArticle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRemoveArticle.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnRemoveArticle.ForeColor = System.Drawing.Color.White;
+            this.BtnRemoveArticle.Location = new System.Drawing.Point(403, 160);
+            this.BtnRemoveArticle.Name = "BtnRemoveArticle";
+            this.BtnRemoveArticle.Size = new System.Drawing.Size(75, 25);
+            this.BtnRemoveArticle.TabIndex = 9;
+            this.BtnRemoveArticle.Text = "Remove";
+            this.BtnRemoveArticle.UseVisualStyleBackColor = true;
+            this.BtnRemoveArticle.Click += new System.EventHandler(this.BtnRemoveArticle_Click);
+            // 
+            // BtnAddArticle
+            // 
+            this.BtnAddArticle.BackColor = System.Drawing.Color.Black;
+            this.BtnAddArticle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAddArticle.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnAddArticle.ForeColor = System.Drawing.Color.White;
+            this.BtnAddArticle.Location = new System.Drawing.Point(322, 160);
+            this.BtnAddArticle.Name = "BtnAddArticle";
+            this.BtnAddArticle.Size = new System.Drawing.Size(75, 25);
+            this.BtnAddArticle.TabIndex = 8;
+            this.BtnAddArticle.Text = "Add link";
+            this.BtnAddArticle.UseVisualStyleBackColor = true;
+            this.BtnAddArticle.Click += new System.EventHandler(this.BtnAddArticle_Click);
             // 
             // AddWordForm
             // 
@@ -273,7 +319,8 @@ namespace sozluk
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.CancelButton = this.BtnCancel;
-            this.ClientSize = new System.Drawing.Size(508, 515);
+            this.ClientSize = new System.Drawing.Size(509, 691);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -298,6 +345,8 @@ namespace sozluk
             this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -307,9 +356,7 @@ namespace sozluk
         private System.Windows.Forms.Button BtnAdd;
         private System.Windows.Forms.Button BtnCancel;
         private System.Windows.Forms.TextBox TxtWordName;
-        private System.Windows.Forms.TextBox TxtArticleLink;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox TxtDefinition;
@@ -322,5 +369,10 @@ namespace sozluk
         private System.Windows.Forms.Button BtnRemoveReference;
         private System.Windows.Forms.Button BtnAddReference;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox TxtArticle;
+        private System.Windows.Forms.ListBox ListArticles;
+        private System.Windows.Forms.Button BtnRemoveArticle;
+        private System.Windows.Forms.Button BtnAddArticle;
     }
 }
